ed1cc025f0aee863e11c5fb6f2cc8a78456453dd
inamyrpl28 <in'amy_fadlyyah_28rpl@student.smktelkom-mlg.sch.id>
2021-04-30 21:16:42 +0700

    first commit

---
+++ app/src/main/java/www/smktelkom/sch/id/MovieResponse.java
@@ -0,0 +1,35 @@
+package www.smktelkom.sch.id;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieResponse{
+	@SerializedName("Response")
+	private String response;
+	@SerializedName("totalResults")
+	private String totalResults;
+	@SerializedName("Search")
+	private List<SearchItem> search;
+
+
+	public String getResponse(){
+		return response;
+	}
+
+	public void setTotalResults(String totalResults){
+		this.totalResults = totalResults;
+	}
+
+	public String getTotalResults(){
+		return totalResults;
+	}
+
+	public void setSearch(List<SearchItem> search){
+		this.search = search;
+	}
+
+	public List<SearchItem> getSearch(){
+		return search;
+	}
+}
